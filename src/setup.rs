59df6d37008676cab8f079cab17b5cd772166081
laserzz <ryan.skeeb@gmail.com>
2023-06-26 01:45:33 -0700

    enhance setup

--- src/setup.rs
+++ src/setup.rs
@@ -17,5 +17,23 @@ pub fn setup() {
             "workers": num_cpus::get_physical()
         })).expect("Failed to write to settings");
         println!("WARNING: No settings.json file was found so the password is empty. It is highly recommended you set one.");
+    } else {
+        let db_file_exists = std::path::Path::new("./dbs/db.json").exists();
+        if !db_file_exists {
+            let db = fs::File::create("./dbs/db.json").expect("Failed to create DB file");
+            to_writer(db, &json!({})).expect("Failed to write to DB");
+        }
+        let settings_file_exists = std::path::Path::new("./dbs/settings.json").exists();
+        if !settings_file_exists {
+            let settings = fs::File::create("./dbs/settings.json").expect("Failed to create settings file");
+            to_writer_pretty(settings, &json!({
+            "address": "127.0.0.1",
+            "port": 8080,
+            "password": "",
+            "snapshot_seconds": 30,
+            "workers": num_cpus::get_physical()
+            })).expect("Failed to write to settings");
+            println!("WARNING: No settings.json file was found so the password is empty. It is highly recommended you set one.");
+        }
     }
 }
