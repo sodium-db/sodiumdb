02f3fbfe366832a04ecc3c99952c26a426e8c413
laserzz <ryan.skeeb@gmail.com>
2023-06-28 03:33:30 -0700

    fix py example

--- examples/python_example.py
+++ examples/python_example.py
@@ -4,12 +4,12 @@ headers = {"Authorization": "myAmazingPassword", "Content-Type": "application/js
 url = 'http://127.0.0.1:8080'
 
 # create
-requests.post(url, headers=headers, json={"hello": "world"})
+requests.post(f"{url}/create", headers=headers, json={"hello": "world"})
 
 # read
-read = requests.post(url, headers=headers, json={"entry": "hello"})
+read = requests.post(f"{url}/read", headers=headers, json={"entry": "hello"})
 assert read.json()["result"] == "world"
 
 # delete
-delete = requests.post(url, headers=headers, json={"entry": "hello"})
+delete = requests.post(f"{url}/delete", headers=headers, json={"entry": "hello"})
 assert delete.json() == {"hello": "world"}
